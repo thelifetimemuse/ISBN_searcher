51360c3fb6f1c8d78fa401dd32a7154f4799f67d
Christian Espinoza <chespinoza@gmail.com>
2024-05-10 21:09:57 +0100

    feat: add new local provider skeleton

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from fastapi import FastAPI
 
-from searcher.provider import openlibrary as provider
-
 
 app = FastAPI()
 
 
 @app.get("/api/v1/books")
-def books(isbn):
+def books(isbn, provider_name):
+    """
+    Endpoint /api/v1/books?isbn=1&provider_name=XXX
+    """
+
+    provider = None
+
+    if provider_name == "openbooks":
+        from searcher.provider import openlibrary as provider
+    elif provider_name == "muse":
+        from searcher.provider import local as provider
+    else:
+        return {"error": "no provider specified"}
+
     result = provider.search_book(isbn)
     return {"response": result}
