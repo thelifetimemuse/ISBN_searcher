b8af13377d2d0130da5b6b83e4060fdd13e7bb40
thelifetimemuse <thelifetimemuse@gmail.com>
2024-05-20 11:11:23 +0100

    Refactor: Implement alternative search provider

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 @app.get("/api/v1/books")
 def books(isbn, provider_name):
     """
-    Endpoint /api/v1/books?isbn=1&provider_name=XXX
+    Endpoint /api/v1/books?isbn=1&provider_name=XXX or /api/v1/books?subject=fiction&provider_name=XXX
     """
 
     provider = None
@@ -16,6 +16,8 @@ def books(isbn, provider_name):
         from searcher.provider import openlibrary as provider
     elif provider_name == "muse":
         from searcher.provider import local as provider
+    elif provider_name == "bookfinder":
+        from searcher.provider import bookfinder as provider
     else:
         return {"error": "no provider specified"}
 
